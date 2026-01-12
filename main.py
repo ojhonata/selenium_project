c4862396c1585678af77351043b99f62a2051bc6
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2026-01-12 18:38:43 -0300

    add biblioteca beautifulsoup

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup
 
 
 def click_button_menu(drive):
@@ -14,22 +15,40 @@ def click_button_menu(drive):
     )
     button_menu.click()
 
+
 def click_button_datils(drive):
     actions = ActionChains(drive)
     button_details = WebDriverWait(drive, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, "//button[normalize-space()='+DETALHES']"))
+        EC.presence_of_all_elements_located(
+            (By.XPATH, "//button[normalize-space()='+DETALHES']")
+        )
     )
 
     for button in button_details:
         try:
-            drive.execute_script('arguments[0].scrollIntoView(true);', button)
-            drive.execute_script('arguments[0].click()', button)
+            drive.execute_script("arguments[0].scrollIntoView(true);", button)
+            drive.execute_script("arguments[0].click()", button)
+
+            html = drive.page_source
+            soup = BeautifulSoup(html, "html.parser")
+
+            modal = soup.find("td", class_="_ngcontent-c8")
+            if modal:
+                title_h4 = soup.find_all("div", class_="product-info")
+                for title_all in title_h4:
+                    if title_all:
+                        title = title_all.text.strip()
+                        print(title)
+                    else:
+                        print("!!!!!")
+            else:
+                print("não encontramos")
 
             time.sleep(2)
-
             actions.send_keys(Keys.ESCAPE).perform()
         except Exception as error:
-            print(f'Erro: {error}')
+            print(f"Erro: {error}")
+
 
 def click_after_button(drive):
     while True:
@@ -37,7 +56,9 @@ def click_after_button(drive):
             click_button_datils(drive)
 
             after = WebDriverWait(drive, 5).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "a[aria-label='Próximo page']"))
+                EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, "a[aria-label='Próximo page']")
+                )
             )
 
             after.click()
@@ -46,10 +67,11 @@ def click_after_button(drive):
         except TimeoutException:
             break
 
+
 def click_category(drive, names_category):
-    pratos = 'Pratos'
+    pratos = "Pratos"
     jump = False
-    
+
     drive.execute_script("window.scrollTo(0, 0);")
 
     for indice, category in enumerate(names_category):
@@ -59,7 +81,9 @@ def click_category(drive, names_category):
         try:
             drive.execute_script("window.scrollTo(0, 0);")
             categories = WebDriverWait(drive, 5).until(
-                EC.element_to_be_clickable((By.XPATH, f"//button[normalize-space()='{category}']"))
+                EC.element_to_be_clickable(
+                    (By.XPATH, f"//button[normalize-space()='{category}']")
+                )
             )
 
             time.sleep(2)
@@ -71,25 +95,41 @@ def click_category(drive, names_category):
                     next_category = names_category[next_index]
 
                     next = WebDriverWait(drive, 5).until(
-                        EC.element_to_be_clickable((By.XPATH, f"//button[normalize-space()='{next_category}']"))
+                        EC.element_to_be_clickable(
+                            (By.XPATH, f"//button[normalize-space()='{next_category}']")
+                        )
                     )
                     next.click()
                     jump = True
-                
+
             click_after_button(drive)
 
         except Exception as e:
-            print(f'Erro ao processar a categoria {category} {e}')
+            print(f"Erro ao processar a categoria {category} {e}")
 
     time.sleep(2)
 
+
 def main():
-    list_category = ['Bebidas', 'Calzones', 'Lanches', 'Petiscos', 'Pizzas', 'Pizzas Doces', 'Pratos', 'Saladas', 'Massas', 'Executivos', 'Prato', 'Peixes']
+    list_category = [
+        "Bebidas",
+        "Calzones",
+        "Lanches",
+        "Petiscos",
+        "Pizzas",
+        "Pizzas Doces",
+        "Pratos",
+        "Saladas",
+        "Massas",
+        "Executivos",
+        "Prato",
+        "Peixes",
+    ]
 
     drive = webdriver.Chrome()
-    drive.get('https://www.dardanella.com.br/inicio')
+    drive.get("https://www.dardanella.com.br/inicio")
     drive.maximize_window()
-    
+
     click_button_menu(drive)
 
     time.sleep(2)
@@ -100,5 +140,5 @@ def main():
     drive.quit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
